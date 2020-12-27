52d9633ac3bd049f5d688c6fc39ceb7d47809b91
Luis Villalobos <luisevm24@outlook.com>
2020-12-27 13:45:04 -0600

    Product controller without middleware

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('products', ProductController::class);
+
+/*
 Route::middleware(['auth'])->group(function () {
     Route::resource('products', ProductController::class);
 });
+*/
